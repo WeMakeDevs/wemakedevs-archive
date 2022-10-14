d28fe44dc7666e1cb2aa347adc39bc8807a47eb1
Bhavya Sachdeva <bhavya.sachdeva.58@gmail.com>
2022-10-14 20:31:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import Header from './components/Header.js'
-import Navbar from './components/navbar/Navbar.js'
 import ComingSoon from './components/ComingSoon.js'
 
 
 const App = () => {
   return (
     <div>
-    <Navbar />
-    <Header />
-    <ComingSoon />
+     <ComingSoon />
     </div>
 
   )
