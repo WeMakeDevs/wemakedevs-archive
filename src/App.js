2bd943b5e813a123548e24931a4be7bca2c363a5
bhavyastar <bhavya.sachdeva.58@gmail.com>
2022-10-17 20:15:40 +0530

    [FEATURE] <Add Podcast>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from './components/Header.js'
 import Navbar from './components/navbar/Navbar.js'
-
+import Podcast from './components/Podcast.js'
 
 const App = () => {
   return (
     <div>
     <Navbar />
     <Header />
+    <Podcast />
   
     </div>
 
