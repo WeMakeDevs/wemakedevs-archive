60c0d5a7509ab2d10d022167857558ef3d4b8d17
Aayush Sharma <aayusharma1401@gmail.com>
2022-10-18 17:14:39 +0530

    Updates

---
+++ precache-manifest.7613fe4dd635881a46ba0ce2d53712fd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e647fcae5cb597113f5",
+    "url": "/commclassroom/static/css/main.c0a26d31.chunk.css"
+  },
+  {
+    "revision": "9e647fcae5cb597113f5",
+    "url": "/commclassroom/static/js/main.9e647fca.chunk.js"
+  },
+  {
+    "revision": "b5e4af83b0b19e43c83d",
+    "url": "/commclassroom/static/js/1.b5e4af83.chunk.js"
+  },
+  {
+    "revision": "3cf512bcf3ae3661f270",
+    "url": "/commclassroom/static/js/runtime~main.3cf512bc.js"
+  },
+  {
+    "revision": "452dbcc47a41d5c940d98f7066fbc075",
+    "url": "/commclassroom/static/media/podcast.452dbcc4.png"
+  },
+  {
+    "revision": "5d4ce39341050ae34780318b43adadea",
+    "url": "/commclassroom/static/media/Founder.5d4ce393.png"
+  },
+  {
+    "revision": "81f5b39d75f7a300202766f9cc0de357",
+    "url": "/commclassroom/static/media/about-us.81f5b39d.png"
+  },
+  {
+    "revision": "ac374f37de34d7f4b664b28162a77729",
+    "url": "/commclassroom/static/media/course.ac374f37.png"
+  },
+  {
+    "revision": "60b5eb7fb84eb5bcbcf2b82ee8346026",
+    "url": "/commclassroom/static/media/event.60b5eb7f.png"
+  },
+  {
+    "revision": "8dde36c62dd0e2d1844adffcfc9a5aa9",
+    "url": "/commclassroom/static/media/mentor.8dde36c6.png"
+  },
+  {
+    "revision": "77fed3530ec954df60bf50b55ca07a5e",
+    "url": "/commclassroom/static/media/blog.77fed353.png"
+  },
+  {
+    "revision": "b73bdf41c6cbbbfa4e183f3e38c2bfc7",
+    "url": "/commclassroom/index.html"
+  }
+];
