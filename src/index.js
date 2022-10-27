86aa1bceb5683145b905adaf08f1c81b447bc6b4
Bhavya Sachdeva <bhavya.sachdeva.58@gmail.com>
2022-10-27 18:38:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
