8517e50ed3317ae344a7737d438641709d343e1a
Bhavya Sachdeva <bhavya.sachdeva.58@gmail.com>
2022-10-27 17:35:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/commclassroom'>
+  <BrowserRouter>
   <App />
   </BrowserRouter>
 );
