e3213b01963857fb565bcd2a10fd04aebb961ccc
Aayush Sharma <aayusharma1401@gmail.com>
2022-10-30 01:06:51 +0530

    Update Event.js

--- src/components/Event.js
+++ src/components/Event.js
@@ -14,7 +14,7 @@ const Event = () => {
           <li>Events conducted will help develop various skills of students <br /> in co-curricular activities and to expose them to the current <br /> trends in the technical and professional fields.</li>
           <li>Explore the plethora of events & have the opportunity to grab <br /> amazing prizes & goodies!</li>
         </ul>
-        <a href="" target="_blank" rel="noreferrer">
+        <a href="https://commclassroom.github.io/commclassroom/event" target="_blank" rel="noreferrer">
           <button id="bt_discord">Explore our Events <img className="arrowright" src={Arrowright} alt="arrowup" /></button>
         </a>
       </div>
