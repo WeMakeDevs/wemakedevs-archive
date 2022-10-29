458eca1350db63dabe68092f472712db1d88318f
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-10-30 00:13:29 +0530

    style: add arrow and fix CTA

--- src/components/Roadmap/Header.js
+++ src/components/Roadmap/Header.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "../../styles/Header.css";
 import { roadmapsData as data } from "./data";
 import "./Roadmap.css";
+import ArrowDown from "../../assets/ArrowDown.png";
 
 const Header = ({ scrollRoadmapRef }) => {
   return (
@@ -17,6 +18,7 @@ const Header = ({ scrollRoadmapRef }) => {
           }
         >
           Explore roadmaps
+          <img src={ArrowDown} alt={"Arrow down"} className="arrow-down"></img>
         </a>
       </div>
     </header>
--- src/components/Roadmap/Mentorship.js
+++ src/components/Roadmap/Mentorship.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { mentorshipData as data } from "./data";
+import Arrowright from "../../assets/Arrowright.png";
 
 const Mentorship = () => {
   return (
@@ -10,9 +11,14 @@ const Mentorship = () => {
           <p>{data.description}</p>
           <a href={data.link} target="_blank" rel="noopener noreferrer">
             Watch video
+            <img
+              src={Arrowright}
+              alt={"Arrow right"}
+              className="arrow-right"
+            ></img>
           </a>
         </div>
-        <img src={data.image} alt={"Mentorship"} />
+        <img src={data.image} alt={"Mentorship"} className="thumbnail" />
       </div>
     </section>
   );
