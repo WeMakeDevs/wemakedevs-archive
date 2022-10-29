19186089268885736265a22c66ce471f5c1b89b8
Bishal Das <bishalhnj127@gmail.com>
2022-10-29 11:04:39 +0530

    updated
    Signed-off-by: Bishal Das <bishalhnj127@gmail.com>

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -14,10 +14,10 @@ const Navbar = () => {
          <img className='nabar_logo_1' src={ Logo } alt="logo" />
       </div>
     <ul className={click ? 'navbar-menu active': 'navbar-menu'}>
-        <li className='navbar-item'><a href="/" className='item'>Home</a></li>
+        <li className='navbar-item'><Link to="/commclassroom"><a href="/" className='item'>Home</a></Link></li>
         <li className='navbar-item'><Link to="/commclassroom/course"><a href='/' className='item'>Courses</a></Link></li>
         <li className='navbar-item'><a href="https://www.youtube.com/playlist?list=PL9gnSGHSqcnqwmKYZ5rHuzqe32Di47KMr" className='item'>Podcasts</a></li>
-        <li className='navbar-item'><a href="/" className='item'>Events</a></li>
+        <li className='navbar-item'><Link to="/commclassroom/event"><a href="/" className='item'>Events</a></Link></li>
         <li className='navbar-item'><a href="https://github.com/commclassroom/roadmaps" className='item'>Roadmaps</a></li>
         <li className='navbar-item'><a href="/" className='item'>About</a></li>
         <a className="navbar_youtube_1" href="https://www.youtube.com/c/KunalKushwaha"><button className="navbar_youtube"> Youtube <img src={Youtube} alt="youtube" /> </button>
