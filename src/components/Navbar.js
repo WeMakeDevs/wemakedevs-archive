c479ee51e388d7b764f21fac942443317d3d6fa7
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2022-11-03 19:41:01 +0530

    fix: Navbar link routing resolved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,11 +14,11 @@ const Navbar = () => {
          <img className='nabar_logo_1' src={ Logo } alt="logo" />
       </div>
     <ul className={click ? 'navbar-menu active': 'navbar-menu'}>
-        <li className='navbar-item'><Link to="/commclassroom">Home</Link></li>
-        <li className='navbar-item'><Link to="/commclassroom/course">Courses</Link></li>
+        <li className='navbar-item'><Link to="/">Home</Link></li>
+        <li className='navbar-item'><Link to="/course">Courses</Link></li>
         <li className='navbar-item'><a href="https://www.youtube.com/playlist?list=PL9gnSGHSqcnqwmKYZ5rHuzqe32Di47KMr">Podcasts</a></li>
-        <li className='navbar-item'><Link to="/commclassroom/event">Events</Link></li>
-        <li className='navbar-item'><Link to="/commclassroom/roadmaps">Roadmaps</Link></li>
+        <li className='navbar-item'><Link to="/event">Events</Link></li>
+        <li className='navbar-item'><Link to="/roadmaps">Roadmaps</Link></li>
         <a className="navbar_youtube_1" href="https://www.youtube.com/c/KunalKushwaha"><button className="navbar_youtube"> Youtube <img src={Youtube} alt="youtube" /> </button>
        </a>
      </ul>
