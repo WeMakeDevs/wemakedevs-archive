6c2a784539d776b6ee0c2df605c4013d4fb4cef9
Enricopro <enridalberton@gmail.com>
2022-11-07 18:06:42 +0100

    Fixed redirect "Podcast" in Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
     <ul className={click ? 'navbar-menu active': 'navbar-menu'}>
         <li className='navbar-item'><Link to="/">Home</Link></li>
         <li className='navbar-item'><Link to="/course">Courses</Link></li>
-        <li className='navbar-item'><a href="https://www.youtube.com/playlist?list=PL9gnSGHSqcnqwmKYZ5rHuzqe32Di47KMr">Podcasts</a></li>
+        <li className='navbar-item'><a href="/#podcast">Podcasts</a></li>
         <li className='navbar-item'><Link to="/event">Events</Link></li>
         <li className='navbar-item'><Link to="/roadmaps">Roadmaps</Link></li>
         <a className="navbar_youtube_1" href="https://www.youtube.com/c/KunalKushwaha"><button className="navbar_youtube"> Youtube <img src={Youtube} alt="youtube" /> </button>
