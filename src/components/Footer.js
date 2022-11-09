0add8987785f460737abbcdede9cd0ec8dd0bc4e
Enricopro <enridalberton@gmail.com>
2022-11-09 13:39:01 +0100

    Code formatted better in Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,36 +6,36 @@ import {Link} from 'react-router-dom'
 const Footer = () => {
   return (
     <div className="footer">
-       <div className="footer-col1">
+      <div className="footer-col1">
   	 		<img src={CC} alt="CC" />
-            <p className="footer-p" >© 2022 Community Classroom · All rights reserved.</p>
+          <p className="footer-p" >© 2022 Community Classroom · All rights reserved.</p>
   	 	</div>
 
-		<div className="footer-col2">
-  	 		<div className="footer-col2-1">
-  	 			<h4>Sitemap</h4>
-  	 			<ul>
-  	 				<li><Link to="/">Home</Link></li>
-  	 				<li><Link to="/#about">About</Link></li>
-  	 				<li><Link to="/hashnode">Blog</Link></li>
-  	 				<li><Link to="/course">Courses</Link></li>
-             		<li><Link to="/roadmaps">Roadmaps</Link></li>
-             		<li><Link to="/event">Events</Link></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div className="footer-col2-2">
-  	 			<h4>Community</h4>
-  	 			<ul>
-  	 				<li><a href="https://www.youtube.com/c/KunalKushwaha/" target="_blank">YouTube</a></li>
-  	 				<li><a href="https://discord.io/commclassroom" target="_blank">Discord</a></li>
-  	 				<li><a href="https://t.me/commclassroom" target="_blank">Telegram</a></li>
-  	 				<li><a href="https://twitter.com/commclassroom/" target="_blank">Twitter</a></li>
-  	 				<li><a href="https://www.instagram.com/commclassroom/" target="_blank">Instagram</a></li>
-                    <li><a href="https://github.com/commclassroom" target="_blank">GitHub</a></li>
-  	 			</ul>
-  	 		</div>
-  	 	</div>
-		   </div>
+      <div className="footer-col2">
+          <div className="footer-col2-1">
+            <h4>Sitemap</h4>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/#about">About</Link></li>
+              <li><Link to="/hashnode">Blog</Link></li>
+              <li><Link to="/course">Courses</Link></li>
+              <li><Link to="/roadmaps">Roadmaps</Link></li>
+              <li><Link to="/event">Events</Link></li>
+            </ul>
+          </div>
+          <div className="footer-col2-2">
+            <h4>Community</h4>
+            <ul>
+              <li><a href="https://www.youtube.com/c/KunalKushwaha/" target="_blank">YouTube</a></li>
+              <li><a href="https://discord.io/commclassroom" target="_blank">Discord</a></li>
+              <li><a href="https://t.me/commclassroom" target="_blank">Telegram</a></li>
+              <li><a href="https://twitter.com/commclassroom/" target="_blank">Twitter</a></li>
+              <li><a href="https://www.instagram.com/commclassroom/" target="_blank">Instagram</a></li>
+              <li><a href="https://github.com/commclassroom" target="_blank">GitHub</a></li>
+            </ul>
+          </div>
+      </div>
+		</div>
   )
 }
 
