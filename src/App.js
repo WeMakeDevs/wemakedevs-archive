ff66c91cb341def0d898c62efe7a58f73e02d35a
maverick-farhan <mdfarhanofficial@protonmail.com>
2022-11-18 23:35:35 +0530

    Aligning Footer Elements for Mobile

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import {Route, Routes} from 'react-router-dom';
-import './App.css'
-import Course from './pages/CoursePage.js'
-import HomePage from './pages/HomePage.js'
-import Events from './pages/EventsPage.js'
-import Roadmaps from './pages/RoadmapsPage'
-import Hashnode from './pages/Hashnode.js'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Course from "./pages/CoursePage.js";
+import HomePage from "./pages/HomePage.js";
+import Events from "./pages/EventsPage.js";
+import Roadmaps from "./pages/RoadmapsPage";
+import Hashnode from "./pages/Hashnode.js";
 
 const App = () => {
   return (
     <div className="main">
-    <Routes>
-      <Route exact path="/" element={<HomePage/>} />
-      <Route path="/course" element={<Course />} />
-      <Route path="/event" element={<Events />} />
-      <Route path="/roadmaps" element={<Roadmaps />} />
-      <Route path="/hashnode" element={<Hashnode />} />
-    </Routes>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/course" element={<Course />} />
+        <Route path="/event" element={<Events />} />
+        <Route path="/roadmaps" element={<Roadmaps />} />
+        <Route path="/hashnode" element={<Hashnode />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
