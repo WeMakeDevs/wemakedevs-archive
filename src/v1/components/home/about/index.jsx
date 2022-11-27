e8ba222f75463ea25361fe47403404f92e7c7d6b
DESKTOP-FJAKAS7\Siddhi <117962421+siddhigate12@users.noreply.github.com>
2022-11-27 19:43:40 +0530

    style: about section

--- src/v1/components/home/about/index.jsx
+++ src/v1/components/home/about/index.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import AboutImage from "../../../assets/home/aboutus.png";
+import styles from "./index.module.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section>
+      <div className={styles['about-container']}>
+        <img className={styles['banner']} src={AboutImage} alt="About us" />
+        <div className={styles['info-container']}>
+          <h2>About us</h2>
+          <p>
+            <ul>
+              <li>
+                We believe that every student, irrespective of their college or
+                branch, can make it big.{" "}
+                <span className={styles["highlight"]}>
+                  {" "}
+                  Community Classroom is an initiative built on this thought.{" "}
+                </span>
+              </li>
+              <li>
+                We provide{" "}
+                <span className={styles["highlight"]}>
+                  hands-on training, mentorship for FREE
+                </span>{" "}
+                and have an
+                <span className={styles["highlight"]}>
+                  inclusive community.
+                </span>
+              </li>
+              <li>
+                Get{" "}
+                <span className={styles["highlight"]}>
+                  expert guidance with career
+                </span>
+                , Open Source, and internships, jobs around the world.
+              </li>
+            </ul>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
