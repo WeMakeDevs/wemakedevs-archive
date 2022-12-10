b670d2102f3ae82ce5c68675b194b9270b155bae
shivaabhishek07 <shivaabhishek71@gmail.com>
2022-12-10 15:57:10 +0530

    Added blogs to navbar

--- src/v2/components/common/navbar/data.js
+++ src/v2/components/common/navbar/data.js
@@ -11,12 +11,6 @@ export const navLinks = [
    external: false,
   },
   {
-    name: "Podcasts",
-    href:
-      "https://www.youtube.com/playlist?list=PL9gnSGHSqcnqwmKYZ5rHuzqe32Di47KMr",
-    external: true,
-  },
-  {
     name: "Events",
     href: "#events",
     external: false,
@@ -24,6 +18,16 @@ export const navLinks = [
   {
     name: "Roadmaps",
     href: "#roadmaps",
+    external: false,
+  },
+  {
+    name: "Blogs",
+    href: "https://blog.wemakedevs.org/"
+    external: true,
+  },
+  {
+    name: "Podcasts",
+    href: "https://www.youtube.com/playlist?list=PL9gnSGHSqcnqwmKYZ5rHuzqe32Di47KMr",
     external: true,
   },
   {
