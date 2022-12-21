7783138a91d7dedc4f0db70c126832473c79368c
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-21 19:04:34 +0530

    chore(code_style): add eslint, pretier and husky for better code style

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: 'none',
+  jsxBracketSameLine: true,
+  printWidth: 100
+};
