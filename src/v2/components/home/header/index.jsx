d94cc7d2ffa78bc47659a12f76a9fc82fab53a31
manish bisht <manishbisth9711@gmail.com>
2022-12-22 13:57:10 +0530

    Feat : Header changes

--- src/v2/components/home/header/index.jsx
+++ src/v2/components/home/header/index.jsx
@@ -6,38 +6,36 @@ import TopRightArrow from "../../../assets/arrows/top-right-mid-arrow.png";
 
 const Header = () => {
   return (
-    <section className={styles["header-container"]}>
-      <div>
-  
-        <h1>
-        Inspire, Educate, Empower
-        </h1>
-        <p className={styles["tag-line"]}>
-          This is a community which provides{" "}
-          <span className={styles["highlight"]}>free hands-on training</span> in
-          various fields of computer science and have an inclusive community
-          focusing on a{" "}
-          <span className={styles["highlight"]}>learn by doing</span> approach.
-        </p>
-        <div className={styles["links"]}>
-          <a
-            href="https://www.youtube.com/@KunalKushwaha/playlists?view=50&sort=dd&shelf_id=3"
-            className={`${styles["link"]} ${styles["link-primary"]}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            See Courses <img src={RightArrow} aria-hidden={true} alt=""></img>{" "}
-          </a>
-          <a
-            href="https://www.youtube.com/@WeMakeDevs"
-            className={`${styles["link"]} ${styles["link-secondary"]}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Visit YouTube{" "}
-            <img src={TopRightArrow} aria-hidden={true} alt=""></img>
-          </a>
-        </div>
+    <section className={styles["header__container"]}>
+      <h1 className={styles["header__heading"]}>
+        <span>Inspire</span>
+        <span>Educate</span>
+        <span>Empower</span>
+      </h1>
+      <p className={styles["tag-line"]}>
+        This is a community which provides
+        <span>free hands-on training</span> in various fields of computer
+        science and have an inclusive community focusing on a
+        <span>learn by doing</span> approach.
+      </p>
+      <div className={styles["links"]}>
+        <a
+          href="https://www.youtube.com/@KunalKushwaha/playlists?view=50&sort=dd&shelf_id=3"
+          className={`${styles["link"]} ${styles["link-primary"]}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See Courses <img src={RightArrow} aria-hidden={true} alt=""></img>
+        </a>
+        <a
+          href="https://www.youtube.com/@WeMakeDevs"
+          className={`${styles["link"]} ${styles["link-secondary"]}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit YouTube
+          <img src={TopRightArrow} aria-hidden={true} alt=""></img>
+        </a>
       </div>
     </section>
   );
