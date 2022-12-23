bb3d3f1194343be89e22be692de00c9ca5089c39
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-23 14:57:29 +0530

    feat: add ArrowLink and ButtonLink component

---
+++ src/components/links/ButtonLink/index.jsx
@@ -0,0 +1,45 @@
+import clsx from 'clsx';
+import { ImSpinner2 } from 'react-icons/im';
+
+import styles from './index.module.css';
+
+import UnstyledLink from '../UnstyledLink';
+
+const ButtonLink = ({
+  children,
+  className,
+  disabled: buttonDisabled,
+  isLoading,
+  variant = 'primary',
+  isDarkBg = false,
+  as,
+  href,
+  ...rest
+}) => {
+  const disabled = isLoading || buttonDisabled;
+  const Component = as || UnstyledLink;
+  return (
+    <Component
+      href={href}
+      disabled={disabled}
+      className={clsx(
+        styles.button,
+        variant === 'primary' && styles.primary,
+        variant === 'outline' && styles.outline,
+        isDarkBg && styles.isDarkBg,
+        isLoading && styles.isLoading,
+        className
+      )}
+      {...rest}
+    >
+      {isLoading && (
+        <div className={styles.loading}>
+          <ImSpinner2 className='animate-spin' />
+        </div>
+      )}
+      {children}
+    </Component>
+  );
+};
+
+export default ButtonLink;
