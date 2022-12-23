1cc05444e727027286aba9b576d3af34c8eb7671
parvej <parvej@2001>
2022-12-23 22:03:41 +0530

    Created Testimonial

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import Events from '../components/home/events';
 import Header from '../components/home/header';
 import Mentorship from '../components/home/Mentorship';
 import Partner from '../components/home/Partner';
+import Testimonials from '../components/home/Testimonial';
 import Banner from '../components/layout/Banner';
 import Footer from '../components/layout/Footer';
 import Navbar from '../components/layout/Header';
@@ -15,6 +16,7 @@ const HomePage = () => {
       <Header />
       <About />
       <Courses />
+      <Testimonials />
       <Events />
       <Mentorship />
       <Partner />
