8e3122b651b30e30239aad7385e3f64c97dd46cb
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-23 15:10:24 +0530

    refactor: change roadmap to testimonials

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,23 @@
 import About from '../components/home/about';
-import Courses from '../components/home/courses';
+import Courses from '../components/home/courses/index.jsx';
 import Events from '../components/home/events';
 import Header from '../components/home/header';
+import Mentorship from '../components/home/Mentorship';
 import Partner from '../components/home/Partner';
-import Roadmap from '../components/home/roadmaps';
 import Banner from '../components/layout/Banner';
 import Footer from '../components/layout/Footer';
 import Navbar from '../components/layout/Header';
+
 const HomePage = () => {
   return (
     <>
       <Navbar />
-      <Header className='header'></Header>
-      <About></About>
-      <Courses></Courses>
-      <Events></Events>
-      <Roadmap></Roadmap>
-      <Partner></Partner>
+      <Header />
+      <About />
+      <Courses />
+      <Events />
+      <Mentorship />
+      <Partner />
       <Footer />
       <Banner />
     </>
