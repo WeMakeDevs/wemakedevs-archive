6d87fbcd8901fbd8b553fe7900fc0b5dbad2d40e
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-12-24 16:25:28 +0530

    feat: stats, faq

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,12 @@ import About from '../components/home/about';
 import Cards from '../components/home/about/cards';
 import Courses from '../components/home/courses/index.jsx';
 import Events from '../components/home/events';
+import FAQ from '../components/home/FAQ';
 import Header from '../components/home/header';
 import JoinUs from '../components/home/JoinUs';
 import Mentorship from '../components/home/Mentorship';
 import Partner from '../components/home/Partner';
+import Stats from '../components/home/Stats';
 import Testimonials from '../components/home/Testimonial';
 import Banner from '../components/layout/Banner';
 import Footer from '../components/layout/Footer';
@@ -17,10 +19,12 @@ const HomePage = () => {
       <Header />
       <About background='gradient' />
       <Cards />
+      <Stats />
       <Courses background='gradient' />
       <Testimonials />
       <Events background='gradient' />
       <Mentorship />
+      <FAQ />
       <Partner background='gradient' />
       <JoinUs />
       <Footer />
