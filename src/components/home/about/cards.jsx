fe7b97df7e3bad4b9acc0f9658193e17c4a3b48d
parvej <parvej@2001>
2023-01-02 11:15:34 +0530

    Updated content of the cards

--- src/components/home/about/cards.jsx
+++ src/components/home/about/cards.jsx
@@ -46,21 +46,21 @@ const data = [
   {
     title: 'Education',
     description:
-      'We provide FREE hands-on training in various fields of computer science and have an inclusive community focussing on a learn by doing approach.',
+      'We provide FREE hands-on training in various fields of computer science and have an inclusive community focussing on a learn by doing approach.Our education programs are designed to be intensive, hands-on, and practical, so you can learn by doing and apply what you learn to real-world projects.',
     link: 'https://www.youtube.com/@KunalKushwaha/playlists?view=50&sort=dd&shelf_id=3',
     btnText: 'View Courses',
   },
   {
     title: 'Events',
     description:
-      'We provide FREE hands-on training in various fields of computer science and have an inclusive community focussing on a learn by doing approach.',
+      "Our events include intensive bootcamps, technical workshops, and networking events. Whether you're just starting out or looking to take your skills to the next level, we have something for everyone. Check out our current events and sign up now to reserve your spot. We look forward to seeing you at an event soon!",
     link: '/#events',
     btnText: 'Check Events',
   },
   {
     title: 'Mentorship',
     description:
-      'We provide FREE hands-on training in various fields of computer science and have an inclusive community focussing on a learn by doing approach.',
+      'Welcome to the mentorship program at WeMakeDevs! Are you an aspiring web developer looking for guidance and support on your journey? Our mentorship program is here to help. Our experienced mentors will work with you one-on-one and provide guidance, support, and advice to help you achieve your goals.',
     link: '/#mentorship',
     btnText: 'View Roadmaps',
   },
