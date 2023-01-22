c0e147441f45f2c61a078b250998aa9e37c4606b
Chandraprakash Darji <prakashchandra3786@gmail.com>
2023-01-23 04:12:34 +0530

    style: Prize section equal spacing

--- src/components/hashnode/prizes/index.jsx
+++ src/components/hashnode/prizes/index.jsx
@@ -1,25 +1,24 @@
+import clsx from 'clsx';
+
 import styles from './index.module.css';
 
 import Hashnode from '../../../assets/hashnode/Special-Prize.png';
 
-const index = () => {
+const Prizes = ({ background }) => {
   return (
-    <section className={styles.hashnodeMain}>
-      <h1 className='h1'> Current event & Prize </h1>
-      <br />
-      <hr
-        className='styled-hr'
-        style={{
-          textAlign: 'center',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        }}
-      />
-      <br />
-      <br />
-      <img className={styles.hashnodeSpecial} src={Hashnode} />
+    <section
+      className={clsx(
+        styles.section,
+        background === 'gradient' ? 'gradient' : 'static-bg'
+      )}
+    >
+      <div className={clsx('layout', styles.layout)}>
+        <h1 className='h1'> Current event & Prize </h1>
+        <hr className={clsx('styled-hr', styles.hr)} />
+        <img className={styles.hashnodeSpecial} src={Hashnode} />
+      </div>
     </section>
   );
 };
 
-export default index;
+export default Prizes;
