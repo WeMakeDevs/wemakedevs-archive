cb62327252ee4cedcf59370a1c52af0acc20f3f1
Chandraprakash Darji <prakashchandra3786@gmail.com>
2023-01-23 04:06:41 +0530

    style(hashnode/hero): renamed Header with hero replaced to index.module.css

---
+++ src/components/hashnode/Hero/index.jsx
@@ -0,0 +1,31 @@
+import clsx from 'clsx';
+import { SiHashnode } from 'react-icons/si';
+
+import styles from './index.module.css';
+
+const Hero = () => {
+  return (
+    <header className={styles.section}>
+      <div className={clsx('layout', styles.layout)}>
+        <h1 className='heading'>
+          <span className='highlight highlight--yelow'>WeMakeDevs </span>{' '}
+          Blogging Challenge
+        </h1>
+        <p className={clsx('h2', styles.hashnode)}>
+          Powered by <SiHashnode className={styles.icon} /> hashnode{' '}
+        </p>
+        <hr className={clsx('styled-hr', styles.hr)} />
+        <p className='h3'>
+          Get a chance to win exciting prizes such as{' '}
+          <strong>MacBooks, iPhones, iPads and more every month 🤩 </strong>
+        </p>
+        <br />
+        <p className='h3'>
+          <span className='highlight highlight--yelow'>Blogging</span> is about
+          being yourself and putting what you have into it.
+        </p>
+      </div>
+    </header>
+  );
+};
+export default Hero;
--- src/components/hashnode/index.js
+++ src/components/hashnode/index.js
@@ -1,4 +1,4 @@
-export { default as Header } from './header';
+export { default as Hero } from './Hero';
 export { default as Offer } from './offer';
 export { default as Prize } from './prizes';
 export { default as Shoutout } from './shoutout';
