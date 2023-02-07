04a35b61c1365d17c2eecda62a9e2daaa813e43c
Sreejit <sreesen2003@gmail.com>
2023-02-07 19:49:16 +0530

    Update CoursesPage.jsx

--- src/pages/CoursesPage.jsx
+++ src/pages/CoursesPage.jsx
@@ -1,7 +1,6 @@
 import { CoursesPageContent } from './content';
 import { About, Courses, Hero, Testimonials } from 'components/coursespage';
 import Layout from 'components/layout';
-import ScrollToTop from '../components/ScrollToTop';
 
 const CoursesPage = () => {
   return (
@@ -12,7 +11,6 @@ const CoursesPage = () => {
         <Courses />
         <Testimonials />
       </Layout>
-      <ScrollToTop />
     </>
   );
 };
