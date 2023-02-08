9116452f97ae85f4115350651654d22c15b7431d
Sreejit <sreesen2003@gmail.com>
2023-02-08 19:08:27 +0530

    Undo changes

--- src/pages/CoursesPage.jsx
+++ src/pages/CoursesPage.jsx
@@ -1,17 +1,15 @@
-import { CoursesPageContent } from './content';
 import { About, Courses, Hero, Testimonials } from 'components/coursespage';
 import Layout from 'components/layout';
+import { CoursesPageContent } from './content';
 
 const CoursesPage = () => {
   return (
-    <>
       <Layout content={CoursesPageContent}>
         <Hero />
         <About />
         <Courses />
         <Testimonials />
       </Layout>
-    </>
   );
 };
 export default CoursesPage;
