831f9278c5c2a58b8c401a8101ce18460a5af1f0
Sreejit <sreesen2003@gmail.com>
2023-02-09 17:43:53 +0530

    Modified the button

--- src/components/ScrollToTop/index.jsx
+++ src/components/ScrollToTop/index.jsx
@@ -1,7 +1,18 @@
 // eslint-disable-next-line
 import styles from './index.module.css';
+import { useEffect, useState } from 'react';
 import { IoIosArrowDropupCircle } from 'react-icons/io';
 export default function ScrollToTop() {
+  const [goToTop, setgoToTop] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 200) {
+        setgoToTop(true);
+      } else {
+        setgoToTop(false);
+      }
+    });
+  }, []);
   const toTop = () => {
     window.scroll({
       top: 0,
@@ -11,9 +22,11 @@ export default function ScrollToTop() {
   };
   return (
     <div className={styles['scroll--to--top--button']}>
-      <button onClick={toTop} className={styles['scroll--btn']}>
-        <IoIosArrowDropupCircle className={styles['btn--image']} />
-      </button>
+      {goToTop && (
+        <button onClick={toTop} className={styles['scroll--btn']}>
+          <IoIosArrowDropupCircle className={styles['btn--image']} />
+        </button>
+      )}
     </div>
   );
 }
