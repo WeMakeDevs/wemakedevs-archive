6d7425287b582b6d7dd233d6021820c35eb49f57
Sreejit <sreesen2003@gmail.com>
2023-02-13 19:55:57 +0530

    ⚒️Created scroll to top button for the site. (#406)
    * Created Scroll to top button.
    
    * Updated index.jsx to include react-icon
    
    * Updated index.module.css to style the icon.
    
    * Update CoursesPage.jsx
    
    * Update Events.jsx
    
    * Update Hashnode.jsx
    
    * Update HomePage.jsx
    
    * Update SingleCoursePage.jsx
    
    * Rendering the button on app.jsx
    
    * Delete scrllBtn.png
    
    * Undo changes
    
    * Undo changes
    
    * Modified the button
    
    * Improved callback function which renders the button
    
    * Changes import statements

---
+++ src/components/ScrollToTop/index.jsx
@@ -0,0 +1,37 @@
+// eslint-disable-next-line
+import styles from './index.module.css';
+import { useEffect, useState } from 'react';
+import { IoIosArrowDropupCircle } from 'react-icons/io';
+export default function ScrollToTop() {
+  const [goToTop, setgoToTop] = useState(false);
+  const showBtn = () => {
+    if (window.scrollY > 200) {
+      setgoToTop(true);
+    } else {
+      setgoToTop(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', showBtn);
+    return () => {
+      window.removeEventListener('scroll', showBtn);
+    };
+  }, []);
+
+  const toTop = () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  };
+  return (
+    <div className={styles['scroll--to--top--button']}>
+      {goToTop && (
+        <button onClick={toTop} className={styles['scroll--btn']}>
+          <IoIosArrowDropupCircle className={styles['btn--image']} />
+        </button>
+      )}
+    </div>
+  );
+}
