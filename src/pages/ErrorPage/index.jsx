48d8a5ac1b39d598d25166b3e7a1a5d0af9d95e8
Varun <varunvaru524@gmail.com>
2023-02-25 22:42:05 +0530

    Updated Not Found Page

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,18 +1,9 @@
-import { useLocation, useNavigate } from 'react-router-dom';
-
 import styles from './index.module.css';
 
 import ArrowLink from '@/components/links/ArrowLink';
 import ButtonLink from '@/components/links/ButtonLink';
 
 const ErrorPage = () => {
-  const currLocation = useLocation();
-  const navigate = useNavigate();
-
-  const backToHome = () => {
-    navigate('/', { state: currLocation, replace: true });
-  };
-
   const content = (
     <>
       <h1 className={styles['highHeading']}>404 !</h1>
@@ -28,9 +19,9 @@ const ErrorPage = () => {
     <section className={styles['errorPageSection']}>
       <div className={styles['container']}>
         {content}
-        <span onClick={backToHome}>
-          <ArrowLink as={ButtonLink}>Back To Home</ArrowLink>
-        </span>
+        <ArrowLink as={ButtonLink} href='/'>
+          Back To Home
+        </ArrowLink>
       </div>
     </section>
   );
