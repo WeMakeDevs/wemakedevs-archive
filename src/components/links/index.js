52f3dad6fa23a69332cad7473a0b5d9c76f2850f
Chandraprakash Darji <prakashchandra3786@gmail.com>
2023-03-02 15:32:06 +0530

    migrate: Bnnaer component

--- src/components/links/index.js
+++ src/components/links/index.js
@@ -1,4 +1,4 @@
-export { default as HeaderLink } from '@/components/layout';
 export { default as ArrowLink } from '@/components/links/ArrowLink';
 export { default as ButtonLink } from '@/components/links/ButtonLink';
+export { default as HeaderLink } from '@/components/links/HeaderLink';
 export { default as UnstyledLink } from '@/components/links/UnstyledLink';
