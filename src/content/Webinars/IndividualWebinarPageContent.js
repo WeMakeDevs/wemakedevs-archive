e528a97ea347a5b6048b6fc476eee6b3cc88dea8
Chandraprakash Darji <prakashchandra3786@gmail.com>
2023-03-07 17:17:42 +0530

    chore: webinar faq upadate

--- src/content/Webinars/IndividualWebinarPageContent.js
+++ src/content/Webinars/IndividualWebinarPageContent.js
@@ -27,38 +27,38 @@ export const IndividualWebinarPageHeader = [
 export const IndividualWebinarPageFAQ = [
   {
     id: 'FAQ1',
-    question: 'How do I stay up to date on the latest updates?',
+    question: 'What will the webinars be about?',
     answer:
-      'Please subscribe to our newsletter to stay up to date on the latest updates.',
+      'The webinars will cover a wide range of topics, from trending technology, to less popular technologies as well. All will have some great real world use cases.',
   },
   {
     id: 'FAQ2',
-    question: 'Is your community beginner friendly?',
+    question: 'How can I stay notified for the upcoming webinars?',
     answer:
-      'Yes! Even non-coders are welcome. Our courses are designed in such a way that anyone can get started with it. Even people who have never programmed before!',
+      'We announce about the webinars on our socials, and discord. We will also be updating this webpage with upcoming webinars.',
   },
   {
     id: 'FAQ3',
-    question: 'How do I join your community?',
+    question: 'Will these webinars be recorded?',
     answer:
-      'Make sure to join our Discord and Telegram channels. You can find us on all socials media platforms as @wemakedevs',
+      "Yes. The webinars will be recorded, and the links to them will be included in the 'Past webinars' section.",
   },
   {
     id: 'FAQ4',
-    question: 'How do I take part in events hosted?',
+    question: 'Will the webinars be interactive?',
     answer:
-      'You can head to our WeMakeDevs events repository to find information and sign up for upcoming events.',
+      'Yes, the webinars will be live and interactive. We encourage you to join, ask questions and get them cleared',
   },
   {
     id: 'FAQ5',
-    question: 'Do you provide notes and assignments?',
+    question: 'Who will be a part of these webinars?',
     answer:
-      "Yes. It's crucial to revise the topics and self practice after watching the tutorials and we provide materials for the same.",
+      'We will be having webinars with industry experts on various technologies. Regardless the topic of discussion, you will learn something new and informative.',
   },
   {
     id: 'FAQ6',
-    question: 'Do you provide mentorship and guidance?',
+    question: 'Are these webinars free to attend?',
     answer:
-      'Yes. That is the highlight of our community. Get expert guidance from industry experts from around the world. You can learn more and get inspired by their stories and roadmaps via the podcast Open Source Cafe.',
+      'Yes. The webinars are, and always will be free for anyone to attend. You do not need to pay anything for attending',
   },
 ];
