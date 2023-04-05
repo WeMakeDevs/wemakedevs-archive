163f759fc3df7ff371c57cbf8dfc1d607e642495
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-04-05 19:14:15 +0530

    feat: napptive hackathon card in events page

--- src/components/PageComp/EventsPage/OngoingEvents.jsx
+++ src/components/PageComp/EventsPage/OngoingEvents.jsx
@@ -1,3 +1,4 @@
+import NapptiveBanner from 'assets/hackathon/napptive/Banner.jpg';
 import Hashnode from 'assets/hashnode/Special-Prize.png';
 
 import DetailCard from '@/components/Cards/DetailCard';
@@ -9,6 +10,12 @@ const OngoingEvents = () => {
         <h1 className='h1'> Ongoing events </h1>
         <hr className='styled-hr my-6' />
         <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
+          <DetailCard
+            description='Do you want to learn new skills, discover how to build and deploy cloud native apps and meet like-minded people? Find this and much more by participating in our hackathon!'
+            image={NapptiveBanner}
+            url='/events/hackathons/napptive'
+            btnText='Know more'
+          />
           <DetailCard
             description='Every month we will be having 4 tracks. You can write one or multiple blogs on the given tracks. The best blogs will receive hashnode swag.'
             image={Hashnode}
