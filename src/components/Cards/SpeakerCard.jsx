61e9bd4fbe6acbd9c47b99af9a3fa3b878b62319
Aayush Sharma <aayusharma1401@gmail.com>
2023-04-11 20:37:35 +0530

    fix: meetup changes

--- src/components/Cards/SpeakerCard.jsx
+++ src/components/Cards/SpeakerCard.jsx
@@ -5,11 +5,13 @@ import UnstyledLink from '@/components/links/UnstyledLink';
 const SpeakerCard = ({ img, name, description, linkedin, twitter }) => {
   return (
     <div className='flex flex-col items-center justify-center overflow-hidden rounded-lg border border-content/50 bg-base-200 py-10'>
-      <img
-        src={img}
-        alt={name + ' profile'}
-        className='block aspect-square w-60 rounded-full'
-      />
+      {img && (
+        <img
+          src={img}
+          alt={name + ' profile'}
+          className='block aspect-square w-60 rounded-full'
+        />
+      )}
 
       <h3 className='h2 mt-4'>{name}</h3>
       <p className='h3 mt-3'>{description}</p>
