67c88b3fcce55c4fd8e01fc0c496aa7b1a8d0b0e
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2023-04-14 16:55:22 +0530

    about section complete

--- src/components/PageComp/OpenSourcePage/About.jsx
+++ src/components/PageComp/OpenSourcePage/About.jsx
@@ -1,68 +1,28 @@
-import AboutImage from 'assets/home/aboutus.png';
-
 import ColumnSection from '@/components/layout/ColumnSection';
 import ListItem from '@/components/Listitem';
-import Stats from '@/components/PageComp/EventsPage/Stats';
 
-const About = () => {
+const About = ({ background = 'static' }) => {
   return (
-    <section>
+    <ColumnSection
+      id='sec-offer'
+      src='https://www.youtube.com/embed/msyGybzCKRs'
+      isVideo
+      title='About'
+      direction='right'
+      background={background}
+    >
+      <ListItem>
+      Open source software is accessible to the public at no cost, allowing anyone to view, modify, and distribute its source code.
+      </ListItem>
+      <ListItem>
+      The open source community is made up of developers and users who work together on software projects through online platforms like GitHub.
+      </ListItem>
+      <ListItem>
+      Open source software is valued for its transparency, flexibility, and security, as the code can be reviewed and modified by anyone.
+      </ListItem>
+      <ListItem>Popular examples of open source software include Linux, Apache, Firefox, and WordPress</ListItem>
       
-      <ColumnSection
-        id='about'
-        as='div'
-        src={AboutImage}
-        title='About us'
-        buttonLinks={[
-          {
-            name: 'Discord',
-            href: 'https://discord.gg/wemakedevs',
-          },
-          {
-            name: 'Telegram',
-            href: 'https://t.me/WeMakeDevs',
-          },
-          {
-            name: 'Newsletter',
-            href: '#newsletter',
-          },
-        ]}
-      >
-        <ListItem>
-          We believe that everyone, irrespective of their college or branch,
-          technical or non-technical background can make it big.{' '}
-          <span className='highlight highlight--white'>
-            {' '}
-            WeMakeDevs is an initiative built on this thought.{' '}
-          </span>
-        </ListItem>
-        <ListItem>
-          We provide{' '}
-          <span className='highlight highlight--white'>
-            hands-on training, mentorship for FREE
-          </span>{' '}
-          and have an{' '}
-          <span className='highlight highlight--white'>
-            inclusive community.
-          </span>
-        </ListItem>
-        <ListItem>
-          Get{' '}
-          <span className='highlight highlight--white'>
-            expert guidance with career
-          </span>
-          , Open Source, and internships, jobs around the world.
-        </ListItem>
-        <ListItem>
-          We are also having various{' '}
-          <span className='highlight highlight--white'>
-            events weekly or monthly.{' '}
-          </span>
-          Do participate in those events to make your skills enhance.
-        </ListItem>
-      </ColumnSection>
-      <Stats />
-    </section>
+    </ColumnSection>
   );
 };
 
--- src/pages/OpenSourcePage.jsx
+++ src/pages/OpenSourcePage.jsx
@@ -1,9 +1,11 @@
 import Layout from '@/components/layout';
 import Header from '@/components/PageComp/OpenSourcePage/Header';
+import About  from '@/components/PageComp/OpenSourcePage/About';
 const OpenSourcePage = ({ content }) => {
   return (
     <Layout content={content}>
      <Header />
+     <About />
     </Layout>
   );
 };
