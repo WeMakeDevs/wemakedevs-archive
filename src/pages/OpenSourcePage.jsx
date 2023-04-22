98ad53e6746c5f1893995dc10eda155ece750047
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2023-04-22 23:55:21 +0530

    Opensourcepage our projects section (#569)
    * open source page header done
    
    * conflicts on header component
    
    * about section complete
    
    * alignment of video section changed
    
    * PR changes
    
    * PR changes lint error
    
    * our projects section complete

--- src/pages/OpenSourcePage.jsx
+++ src/pages/OpenSourcePage.jsx
@@ -2,11 +2,16 @@ import Layout from '@/components/layout';
 
 import Header from '@/components/PageComp/OpenSourcePage/Header';
 import About  from '@/components/PageComp/OpenSourcePage/About';
+
+import OurProjects from '@/components/PageComp/OpenSourcePage/OurProjects';
+
 const OpenSourcePage = ({ content }) => {
   return (
     <Layout content={content}>
      <Header />
      <About />
+
+     <OurProjects />
 
     </Layout>
   );
