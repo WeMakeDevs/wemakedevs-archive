613300fac16ccbfdc1504c7e137182f2a66b0238
Siddhant Khisty <siddhantkhisty@gmail.com>
2023-06-03 10:38:33 +0530

    Update: Challenge content

--- src/components/PageComp/HashnodePage/Tracks.jsx
+++ src/components/PageComp/HashnodePage/Tracks.jsx
@@ -27,27 +27,27 @@ export default Tracks;
 const hashnodeData = [
   {
     description:
-      'Write a blog on the topic Development, share some of your best experiences and stand a chance to win the prize.',
+      'Write a blog on the topic DevOps, share some of your learnings, and tools that you use and stand a chance to win the prize.',
     image: Track1,
   },
   {
     description:
-      'Write a blog on the topic Open source, share some of your tips and stand a chance to win prize.',
+      'Write a blog on the topic DSA, share some of your tips, and learnings and stand a chance to win prize.',
     image: Track2,
   },
   {
     description:
-      'Write a blog on the topic Computer Networking and stand a chance to win the prize.',
+      'Write a blog on the topic Embedded Technologies, and stand a chance to win the prize.',
     image: Track3,
   },
   {
     description:
-      'Write a blog on your KubeCon experience and definitely stand a chance to win the grand prize.',
+      'Write a blog on the topic Backend Development, share some of your knowledge and learnings, and stand a chance to win the prize.',
     image: Track4,
   },
   {
     description:
-      'Write a blog sharing about your experience with the Hashnode AI tools and you can win Nike Air Jordans this month!',
+      'Record a video, sharing how participating in the blogging challenges and writing on hashnode has helped you in your learnings and brand growth, share it on Twitter, tagging @WeMakeDevs and @hashnode, and get a change to win a StreamDeck and a YouTube mentorship session with Kunal Kushwaha',
     image: Special,
   },
 ];
