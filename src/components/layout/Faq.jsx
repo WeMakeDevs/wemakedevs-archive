9ee97541b4f5be78cce2d8d60757ef1c388ca852
ANUPAM SHARMA <91144139+007AnupamSharma@users.noreply.github.com>
2023-06-03 19:21:07 +0530

    Update Faq.jsx
    fixed it ... in small devices, either one of those 6 needs to open, and after clicking on another accordion the previous one needs to be closed .

--- src/components/layout/Faq.jsx
+++ src/components/layout/Faq.jsx
@@ -1,14 +1,35 @@
 import Accordion from '@/components/Accordion';
-
+import { useState, useEffect } from 'react';
 const Faq = ({ faq }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
     <section id='faq'>
       <div className='layout mx-auto py-20'>
         <h2 className='h1'>FAQs</h2>
         <hr className='styled-hr my-6' />
         <div className='grid-colos-1 grid gap-8 lg:grid-cols-2 lg:gap-6'>
-          <Accordion data={faq.slice(0, 3)}></Accordion>
-          <Accordion data={faq.slice(3, 6)}></Accordion>
+        {windowWidth<1024? 
+        (
+        <>
+        <Accordion data={faq.slice(0, 6)}></Accordion>
+        </>
+        ) 
+        : (
+        <>
+        <Accordion data={faq.slice(0, 3)}></Accordion>
+        <Accordion data={faq.slice(3, 6)}></Accordion>
+        </>
+        )}
         </div>
       </div>
     </section>
