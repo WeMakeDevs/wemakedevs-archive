3c88cd1e82b2d2fc00464ae8a5444117bfe3e314
ANUPAM SHARMA <91144139+007AnupamSharma@users.noreply.github.com>
2023-06-11 15:26:33 +0530

    Update Faq.jsx

--- src/components/layout/Faq.jsx
+++ src/components/layout/Faq.jsx
@@ -1,35 +1,25 @@
 // eslint-disable-next-line
 import Accordion from '@/components/Accordion';
-import { useState, useEffect } from 'react';
-// FAQ Component
+import AccordionItem from '@/components/Accordion/AccordionItem';
+import { useState } from 'react';
 const Faq = ({ faq }) => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
+  const [activeItem, setActiveItem] = useState('-1');
   return (
     <section id='faq'>
       <div className='layout mx-auto py-20'>
         <h2 className='h1'>FAQs</h2>
         <hr className='styled-hr my-6' />
-        <div className='grid-colos-1 grid gap-8 lg:grid-cols-2 lg:gap-6'>
-          {windowWidth < 1024 ? (
-            <>
-              <Accordion data={faq.slice(0, 6)}></Accordion>
-            </>
-          ) : (
-            <>
-              <Accordion data={faq.slice(0, 3)}></Accordion>
-              <Accordion data={faq.slice(3, 6)}></Accordion>
-            </>
-          )}
+        <div className='grid-colos-1 grid gap-8 lg:gap-6'>
+          {faq.map((item, index) => (
+            <AccordionItem
+              key={item.id}
+              index={index}
+              question={item.question}
+              answer={item.answer}
+              activeItem={activeItem}
+              setActiveItem={setActiveItem}
+            />
+          ))}
         </div>
       </div>
     </section>
