50b60cbc27765e885cf29dd14b036c4de56c89f9
Susmita Dey <susmitadey475@gmail.com>
2023-06-21 14:24:05 +0530

    Add Scholarship testimonials (#670)
    * add testimonial component for scholarships
    
    * Add Scholarship testimonials
    
    * Add Source and divide into paras

---
+++ src/content/Scholarship/testimonial.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    src: '/assets/images/scholarship/Tarun.jpg',
+    desc1: `WeMakeDevs made my Kubecon experience unforgettable. I was fortunate enough to receive a scholarship from them, which allowed me to attend this incredible event. The opportunities and knowledge I gained were beyond expectations.`,
+    desc2: `I shared my experience in detail on my blog, highlighting the valuable insights I gathered during Kubecon. I'm truly grateful to WeMakeDevs for their support and for making it possible for me to be a part of this enriching conference.`,
+    from: 'TARUN CHAWLA',
+    source: 'https://tarunchawla.hashnode.dev/my-kubecon-experience',
+  },
+  {
+    src: '/assets/images/scholarship/Diana.jpg',
+    desc1: `Attending Kubecon was a dream come true, thanks to WeMakeDevs. Their scholarship made it possible for me to immerse myself in the world of Kubernetes and learn from the industry's best. I shared my experience on Instagram, capturing the exciting moments and key takeaways from the event.`,
+    desc2: `WeMakeDevs' support has been instrumental in my growth and development as a professional in the tech industry. Thank you for this incredible opportunity!`,
+    from: 'DIANA PHAM',
+    source: 'https://www.instagram.com/p/CkaCJChuQV5/?igshid=MzRlODBiNWFlZA==',
+  },
+  {
+    src: '/assets/images/scholarship/Ashwin.jpg',
+    desc1: `I had an amazing time at Kubecon, all thanks to WeMakeDevs and their scholarship program. Being able to attend this event was a game-changer for me. I got to network with experts, learn about the latest trends in Kubernetes, and broaden my knowledge in cloud-native technologies.`,
+    desc2: `I expressed my gratitude on Twitter, showcasing how WeMakeDevs' support has been instrumental in my career growth. Thank you for empowering aspiring developers like me!`,
+    from: 'ASHWIN KUMAR UPPALA',
+    source: 'https://twitter.com/ashwinexe/status/1567172540820520963?s=20',
+  },
+];
+
+export default data;
