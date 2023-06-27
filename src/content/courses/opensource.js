656143765696ef7a74839aae9ed23dff54fd81b8
Chandraprakash-Darji <prakashchandra3786@gmail.com>
2023-06-27 12:41:38 +0530

    feat: open source course page

---
+++ src/content/courses/opensource.js
@@ -0,0 +1,105 @@
+const OpenSourcePageContent = {
+  header: [
+    {
+      name: 'About',
+      href: '#sec-about',
+    },
+    {
+      name: 'Structure',
+      href: '#sec-structure',
+    },
+    {
+      name: 'Testimonials',
+      href: '#testimonials',
+    },
+    {
+      name: 'Join',
+      href: '#join',
+    },
+    {
+      name: 'FAQ',
+      href: '#faq',
+    },
+  ],
+
+  CTA: {
+    name: 'Register',
+    href: '#register',
+  },
+
+  videoLink: 'https://www.youtube.com/embed/rZ41y93P2Qo',
+  structure: {
+    points: [
+      'Regular structured lectures that you can follow along',
+      'Interaction with industry experts to get you started in various domains on tech, learn from the maintainers directly',
+      'Assignments will be updated on the GitHub repo',
+      'We will also share free structured learning resources for various tech stacks, link our roadmaps repo for this.',
+      'Support in applying to open source programs like Google Summer of Code, Outreachy, Major League Hacking Fellowship, LFX, and more.',
+    ],
+    btns: [
+      {
+        text: 'Playlist',
+        link: 'https://www.youtube.com/playlist?list=PL9gnSGHSqcnq3Px3IAYF7yxeWGWrSdHK8',
+      },
+      {
+        text: 'Assignments',
+        link: 'https://github.com/WeMakeDevs/open-source-course',
+      },
+    ],
+  },
+  learninpublic: [
+    {
+      title:
+        'Use #OpenSourceWithKunal on Twitter and share your journey regularly',
+      link: null,
+    },
+    {
+      title:
+        'There will be a blogging challenge will be running along with this and win exciting prices.',
+      link: {
+        title: 'Blogging challenges',
+        href: '/events/hashnode',
+      },
+    },
+  ],
+  faq: [
+    {
+      id: 'FAQ1',
+      question: 'Are all of your courses free?',
+      answer:
+        'Yes! All of our courses are and will always be free as well as high quality. You can find those on our YouTube channel.',
+    },
+    {
+      id: 'FAQ2',
+      question: 'Is your community beginner friendly?',
+      answer:
+        'Yes! Even non-coders are welcome. Our courses are designed in such a way that anyone can get started with it. Even people who have never programmed before!',
+    },
+    {
+      id: 'FAQ3',
+      question: 'How do I join your community?',
+      answer:
+        'Make sure to join our Discord and Telegram channels. You can find us on all socials media platforms as @wemakedevs',
+    },
+    {
+      id: 'FAQ4',
+      question: 'How do I get my doubts resolved?',
+      answer:
+        'Join our Discord community along with thousands of others and feel free to ask your doubts in the relevant channels.',
+    },
+    {
+      id: 'FAQ5',
+      question: 'Do you provide notes and assignments?',
+      answer:
+        "Yes. It's crucial to revise the topics and self practice after watching the tutorials and we provide materials for the same.",
+    },
+    {
+      id: 'FAQ6',
+      question: 'Do you provide mentorship and guidance?',
+      answer:
+        'Yes. That is the highlight of our community. Get expert guidance from industry experts from around the world. You can learn more and get inspired by their stories and roadmaps via the podcast Open Source Cafe.',
+    },
+  ],
+};
+
+export default OpenSourcePageContent;
---
+++ src/pages/OpenSourceCoursePage.jsx
@@ -0,0 +1,190 @@
+import { useState } from 'react';
+import { MdOutlineLabelImportant } from 'react-icons/md';
+import { VscDebugBreakpointLog } from 'react-icons/vsc';
+
+import Layout, { ColumnSection, Testimonials } from '@/components/layout';
+import { ArrowLink, ButtonLink } from '@/components/links';
+import ListItem from '@/components/Listitem';
+
+import OpenSourceHeroImg from '@/assets/courses/opensource.avif';
+
+const OpenSourcePage = ({ content }) => {
+  const [tweetId, setTweetId] = useState([
+    '1537416341245681665',
+    '1656911832811184129',
+    '1656561217442562051',
+    '1656959959450394624',
+    '1656141618129547264',
+    '1658437060833968129',
+    '1655812888387530753',
+  ]);
+
+  return (
+    <Layout content={content}>
+      {/* About section */}
+
+      <div className='mt-24 mb-10'>
+        <div className='layout'>
+          <div className='relative shadow-xl sm:overflow-hidden sm:rounded-2xl'>
+            <div className='absolute inset-0'>
+              <img
+                className='h-full w-full object-cover opacity-50'
+                src={OpenSourceHeroImg}
+                alt='People working on laptops'
+              />
+              <div className='absolute inset-0 bg-primary mix-blend-multiply' />
+            </div>
+            <div className='relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8'>
+              <h1 className='text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl'>
+                <span className='block text-content'>
+                  Unlocking the Potential of
+                </span>
+                <span className='block text-indigo-200'>Open Source</span>
+              </h1>
+              <p className='mx-auto mt-6 max-w-lg text-center text-xl text-indigo-200 sm:max-w-3xl'>
+                At Open Source Course, we believe in the power of collaboration
+                and the immense value it brings to the world of technology. Our
+                mission is to empower developers like you by providing
+                comprehensive open source education, connecting you with
+                industry experts, and guiding you through the exciting journey
+                of contributing to real-world projects. Whether you're a
+                seasoned developer or just starting out, our course is designed
+                to enhance your skills, boost your confidence, and open doors to
+                endless opportunities in the tech industry.
+              </p>
+              <div className='mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center'>
+                <ButtonLink href='#sec-structure' as={ArrowLink}>
+                  Course structure
+                </ButtonLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Why Should I Join */}
+      <ColumnSection
+        id='sec-about'
+        src='https://www.youtube.com/embed/msyGybzCKRs'
+        isVideo
+        title='About'
+        direction='left'
+      >
+        <ListItem>
+          Open source is the driving force behind many of the technologies we
+          use today.
+        </ListItem>
+        <ListItem>
+          It fosters collaboration, encourages knowledge sharing, and fuels
+          innovation.
+        </ListItem>
+        <ListItem>
+          By participating in open source projects, you not only gain valuable
+          technical skills but also develop crucial soft skills such as
+          communication, teamwork, and problem-solving.
+        </ListItem>
+        <ListItem>
+          Additionally, contributing to open source can boost your visibility in
+          the developer community, attract potential employers, and create a
+          strong foundation for your future career.
+        </ListItem>
+        <ListItem>
+          Join us to unlock the endless possibilities that open source offers.
+        </ListItem>
+      </ColumnSection>
+
+      {/* Course Strucure */}
+      <section className='relative isolate overflow-hidden'>
+        <div className='layout py-20 text-center'>
+          <h2 className='h1 mb-4'>Comprehensive course structure</h2>
+          <p className='h4 mx-auto mt-2 max-w-2xl font-normal'>
+            Your potential through structured lectures, expert guidance, and
+            real-world assignments
+          </p>
+          <hr className='styled-hr mx-auto my-6' />
+          <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+            {content.structure.points.map((item) => (
+              <div className='rounded-xl bg-base-200 p-8 text-left' key={item}>
+                <VscDebugBreakpointLog
+                  style={{ fill: 'url(#blue-gradient)' }}
+                  className='h2'
+                />
+                <h3 className='mt-5 font-normal'>{item}</h3>
+              </div>
+            ))}
+          </div>
+          {content.structure.btns && (
+            <div className='mt-6 flex flex-wrap items-center justify-center gap-4'>
+              {content.structure.btns.map((btn) => (
+                <ButtonLink as={ArrowLink} href={btn.link} key={btn.text}>
+                  {btn.text}
+                </ButtonLink>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+
+      <section id='sec-learninpublic' className='overflow-x-hidden'>
+        <div className='layout py-20 text-center'>
+          <h2 className='h1'>
+            Learn in public: Share your journey and grow as a developer
+          </h2>
+          <p className='h4 mt-2 font-normal'>
+            Embrace transparency, build connections, and elevate your skills
+            through #OpenSourceWithKunal
+          </p>
+          <hr className='styled-hr mx-auto my-6' />
+
+          <ul className='mx-auto max-w-5xl space-y-5'>
+            {content.learninpublic.map((item) => (
+              <li
+                className='h3 flex w-full gap-3 rounded-xl bg-base-200 py-6 px-4 text-left font-medium'
+                key={item.title}
+              >
+                <MdOutlineLabelImportant
+                  style={{ fill: 'url(#blue-gradient)' }}
+                  className='h2 inline-block text-primary'
+                />
+                <span>
+                  {' '}
+                  {item.title}{' '}
+                  {item.link && (
+                    <ArrowLink
+                      href={item.link.href}
+                      className='inline-flex items-center text-primary'
+                    >
+                      {item.link.title}
+                    </ArrowLink>
+                  )}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+
+      <Testimonials
+        tweetId={tweetId}
+        setTweetId={setTweetId}
+        title='Open Source Testimonials'
+      />
+
+      <section id='register'>
+        <div className='mx-auto flex flex-col items-center justify-center gap-4 py-20 text-center'>
+          <h2 className='h1'>Register for course</h2>
+          <hr className='styled-hr' />
+          <iframe
+            src='https://wemakedevs-newsletter.vercel.app/opensource-course.html'
+            width='100%'
+            style={{ border: '0' }}
+            height={700}
+            title='Register for the course'
+          ></iframe>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default OpenSourcePage;
