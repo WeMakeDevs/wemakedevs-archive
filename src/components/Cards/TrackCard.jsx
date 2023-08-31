179a334c5510fc92059f44c529e961d1f8fa53bc
Chandraprakash-Darji <prakashchandra3786@gmail.com>
2023-08-31 21:40:01 +0530

    style: better track design

--- src/components/Cards/TrackCard.jsx
+++ src/components/Cards/TrackCard.jsx
@@ -1,12 +1,12 @@
 const TrackCard = ({ image, title, description }) => {
   return (
-    <div className='overflow-hidden rounded-lg border border-content/50 bg-gradient-to-br from-transparent to-base-100/50'>
+    <div className='overflow-hidden rounded-2xl bg-content/5 p-4'>
       <img
         src={image}
-        className='aspect-[1145/720] w-full object-cover object-center'
+        className='aspect-[1145/720] w-full rounded-lg object-cover object-center'
         alt={title + 'banner'}
       />
-      <div className='p-6'>
+      <div className='pt-6 pb-3'>
         <h3>{title}</h3>
         <p className='mt-2'>{description}</p>
       </div>
