b313fdedf6bad766e5a2420f3998ba95a3b219c7
HARSH TIWARI <harsh.official1802@gmail.com>
2023-10-02 17:31:17 +0530

    [Fix] bug Issue #741

--- src/components/layout/JoinUs/NewsLetter.jsx
+++ src/components/layout/JoinUs/NewsLetter.jsx
@@ -9,14 +9,14 @@ const NewsLetter = () => {
     if (windowWidth < 900) {
       setIframeHeight(830);
     } else if (windowWidth < 1250) {
-      setIframeHeight(700);
+      setIframeHeight(800);
     } else {
-      setIframeHeight(670);
+      setIframeHeight(740);
     }
   }, []);
 
   return (
-    <div id='newsletter'>
+    <div id='newsletter' className='overflow-hidden'>
       <div className='mx-auto flex flex-col items-center justify-center gap-4 text-center'>
         <h2 className='h1'>Subscribe to our newsletter</h2>
         <hr className='styled-hr' />
