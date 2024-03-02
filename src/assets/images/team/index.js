b609f92b5ab164bcf65614b4c25161a7f35963e1
Adarsh Dubey <dubeyadarshmain@gmail.com>
2024-03-02 22:19:31 +0530

    feat: add images for team section

--- src/assets/images/team/index.js
+++ src/assets/images/team/index.js
@@ -1,3 +1,7 @@
+import aayushImage from './aayush.webp';
 import adarshImage from './adarsh.webp';
+import bhavyaImage from './bhavya.webp';
+import gauriImage from './gauri.webp';
+import kunalImage from './kunal.webp';
 
-export { adarshImage };
+export { aayushImage, adarshImage, bhavyaImage, gauriImage, kunalImage };
